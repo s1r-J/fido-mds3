f51916311f98f48fc17a8bb2d88475d7a080c4c5
s1r-J <s1r.aria.azureglo@gmail.com>
2021-12-21 23:57:41 +0900

    Fix update.js

--- .github/workflows/update.js
+++ .github/workflows/update.js
@@ -1,14 +1,19 @@
+const axios = require('axios');
 const fs = require('fs');
 const FM3 = require('../../index');
 
-const client = new FM3.Builder().build();
-client.refresh().then(() => {
-  const json = JSON.parse(fs.readFileSync('../../package.json', 'utf8'));
+(async () => {
+  const mdsResponse = await axios.get('https://mds.fidoalliance.org/');
+  fs.writeFileSync('./data/blob.jwt', mdsResponse.data, 'utf-8');
 
-  const date = new Date.toISOString().slice(0, 10).replace(/-/g, '');
+  const accessor = FM3.Accessor;
+  await accessor.setRootCertUrl('http://secure.globalsign.com/cacert/root-r3.crt');
+  await accessor.fromJwt(mdsResponse.data);
+  await accessor.toFile('./data/payload.json');
+
+  const json = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
   const version = json.version.substring(0, 5).concat(`-${date}`);
   json.version = version;
-  fs.writeFileSync('../../package.json', JSON.stringify(json, null, 2));
-}).catch((err) => {
-  throw err;
-});
+  fs.writeFileSync('./package.json', JSON.stringify(json, null, 2));
+})();
